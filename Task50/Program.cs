c519538beffcc4c4a868f3dd02f6bf427583f453
ALex <sknnov@yandex.ru>
2022-12-04 22:53:25 +0300

    Task 52

---
+++ Task50/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+int InputInt(string letter){
+
+    Console.Write($"Введите {letter}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+
+int[,] GetFillArray(int m, int n){
+
+    int[,] arr = new int[m,n];
+    for (int i=0; i < arr.GetLength(0); i++){
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = new Random().Next(-100, 101);
+        }
+    }
+    
+    return arr;
+}
+
+
+void PrintArray(int[,] arr){
+
+    for (int i = 0; i < arr.GetLength(0); i++){
+        for (int j = 0; j < arr.GetLength(1); j++){
+            
+            Console.Write($"{arr[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindInArray(int[,] arr, int findNumber){
+
+    int key = 0 ;
+    for (int i = 0; i < arr.GetLength(0); i++){
+        for (int j = 0; j < arr.GetLength(1); j++){
+            
+            if(arr[i, j] == findNumber){
+                Console.WriteLine($"Координаты числа: {i+1}, {j+1}");
+                key = 1;
+            }
+        }
+    }
+
+    if(key == 0) Console.WriteLine("Такого числа нет");
+}
+
+Console.Clear();
+int m = InputInt("m");
+int n = InputInt("n");
+int[,] arr = GetFillArray(m,n);
+
+PrintArray(arr);
+Console.WriteLine();
+int findNumber = InputInt("Число");
+FindInArray(arr, findNumber);
+Console.WriteLine();
+
+
+
+
---
+++ Task52/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int InputInt(string letter){
+
+    Console.Write($"Введите {letter}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+
+int[,] GetFillArray(int m, int n){
+
+    int[,] arr = new int[m,n];
+    for (int i=0; i < arr.GetLength(0); i++){
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = new Random().Next(-10, 11);
+        }
+    }
+    
+    return arr;
+}
+
+
+void PrintArray(int[,] arr){
+
+    for (int i = 0; i < arr.GetLength(0); i++){
+        for (int j = 0; j < arr.GetLength(1); j++){
+            
+            Console.Write($"{arr[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void AverageInArray(int[,] arr){
+
+    for (int i = 0; i < arr.GetLength(1); i++){
+        
+        double average = 0 ;
+        for (int j = 0; j < arr.GetLength(0); j++){
+            
+            average += arr[j,i];
+        }
+        average = Math.Round(average / arr.GetLength(0),1);
+        Console.Write($"{average}\t");
+    }
+}
+
+Console.Clear();
+int m = InputInt("m");
+int n = InputInt("n");
+int[,] arr = GetFillArray(m,n);
+
+PrintArray(arr);
+Console.WriteLine();
+
+AverageInArray(arr);
